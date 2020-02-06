2864b5e7a2f841aad3f0df34da1930076f2d3007
Elizabeth Hernandez <eli.hdz.lora@gmail.com>
2020-02-06 15:42:21 -0600

    Slack functions

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from keras.models import load_model
+from slackclient import SlackClient
+
+import keras.backend as kb
+import keras.backend.tensorflow_backend as tb
 
 import tensorflow as tf
 import numpy as np
@@ -9,6 +13,7 @@ import json
 import pickle
 import random
 import spacy
+import requests
 
 data_json = 'intents.json'
 words_file = 'words.pkl'
@@ -48,8 +53,10 @@ def bow(sentence, words, show_details=False):
 def predict_class(sentence):
     # filter out predictions below a threshold
     p = bow(sentence, words)
+    
     file_model = 'chatbot_model.h5'
     model = load_model(file_model)
+    
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
@@ -74,31 +81,70 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-# Endpoint
+# Http Endpoint
 @csrf_exempt
 def get_answer(request):
 
-    import keras.backend as kb
     kb.clear_session()
-    # import keras.backend.tensorflow_backend as tb
-    # from slackclient import SlackClient
-    # tb._SYMBOLIC_SCOPE.value = True
-    
+    tb._SYMBOLIC_SCOPE.value = True
+        
     msg = request.POST.get("msg")
-
     res = chatbot_response(msg)
-    
 
     if not res:
         return JsonResponse({'success': False, 'answer': ""})
 
-    # Write in Slack
-    # token = "xoxb-918589458594-931400580288-4ihl4bSqPBXwktRH66e5h9eP"
-    # slack_client = SlackClient(token)
-    # slack_client.api_call("chat.postMessage", channel="#general", text="Important Message")
-
     return JsonResponse({'success': True, 'answer': res})
 
+# Slack Implementation
+# One channel
+def slackConversation():
+    res = None
+    token = "xoxb-918589458594-931400580288-84vZ9LkMm2GTviF4WMNfHqE3"
+    
+    conv_hist = "https://slack.com/api/conversations.history?token=" + token + "&channel=CTQDM3SLW&inclusive=true&limit=1"
+    response_msgs = make_request(conv_hist)
+    
+    msgs = response_msgs.get('messages', False)
+
+    if msgs:
+        if msgs[0].get('client_msg_id', False):
+            msg = msgs[0].get('text')
+            res = chatbot_response(msg)
+        
+    # Write in Slack
+    if res is not None:
+        slack_client = SlackClient(token)
+        slack_client.api_call("chat.postMessage", channel="#robochat", text=res)
+
+# Reponse to more than one channel
+def slackConversations():
+    pass
+    # # Getting channels
+    # conv_list_url = "https://slack.com/api/conversations.list?token=" + token
+    # resp_conv_list = make_request(conv_list_url)
+    
+    # # Getting id and name for every channel
+    # if resp_conv_list.get('ok', False):
+    #     bot_channels = [{
+    #         'id': c.get('id'),
+    #         'channel': c.get('name')
+    #     } for c in resp_conv_list.get('channels', [])]
+
+    # # Getting the last message of every channel
+    # response_msgs = []
+    # for channel in bot_channels:
+    #     conv_hist = "https://slack.com/api/conversations.history?token=" + token + "&channel=" + channel['id'] + "&inclusive=true&limit=1"
+    #     response_msgs.append(make_request(conv_hist))
+
+def make_request(url):
+    req = requests.get(url)
+    req_data = json.loads(req.text)
+    
+    return req_data
+
+slackConversation()
+# Simple build
 # query = "Hello my friend"
 # print("We need to receive this in a json request: %s" % query)
 # print("We need to send this in a json response: %s" % chatbot_response(query))
